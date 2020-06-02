74b8d3cea8fd3c54deace80277a0125d45489c94
Marcelo Guimarães <celoguimaraes@gmail.com>
2020-06-01 23:28:00 -0300

    Adicionando o Service Discovery

--- order-service-producer/src/main/java/com/guimasoftware/OrderRequestApplication.java
+++ order-service-producer/src/main/java/com/guimasoftware/OrderRequestApplication.java
@@ -5,7 +5,9 @@ import com.guimasoftware.interfaces.stream.Channels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //@EnableEurekaClient também pode ser utilizado
 @EnableBinding(Channels.class)
 @SpringBootApplication
 public class OrderRequestApplication {
--- service-discovery/src/main/java/com/guimasoftware/EurekaServiceApplication.java
+++ service-discovery/src/main/java/com/guimasoftware/EurekaServiceApplication.java
@@ -3,8 +3,6 @@ package com.guimasoftware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Profile;
-
 
 @SpringBootApplication
 @EnableEurekaServer
